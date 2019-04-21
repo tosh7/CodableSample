66027f0348ec895755a3e48a4f5062228cbd0e84
Satoshi Komatsu <satoshikomatsu@SatoshinoMacBook-Pro.local>
2019-04-21 20:07:03 +0900

    artcleModelを作成

---
+++ CodableSample/Article.swift
@@ -0,0 +1,14 @@
+//
+//  Article.swift
+//  CodableSample
+//
+//  Created by Satoshi Komatsu on 2019/04/21.
+//  Copyright © 2019 Satoshi Komatsu. All rights reserved.
+//
+
+import Foundation
+
+struct Article {
+    var title = ""
+    var userId = ""
+}
