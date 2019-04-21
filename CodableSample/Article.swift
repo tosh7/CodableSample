41b175d69ca13eabd0dfc3a2a521c99464241f3e
tosh7 <zlia.6.lj.425@gmail.com>
2019-04-22 01:46:03 +0900

    made article class

--- CodableSample/Article.swift
+++ CodableSample/Article.swift
@@ -9,8 +9,24 @@
 import Foundation
 
 struct Article {
+    
     var title = ""
     var userId = ""
     
+}
+
+extension Article {
     
+    init(_ json: [String: Any]) {
+        
+        if let title = json["title"] as? String {
+            self.title = title
+        }
+        
+        if let user = json["user"] as? [String: Any] {
+            if let userId = user["id"] as? String {
+                self.userId = userId
+            }
+        }
+    }
 }
